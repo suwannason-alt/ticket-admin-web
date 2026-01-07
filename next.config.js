33b67c5a28634df4fa65bca22176c0cff3f93aa4
suwannason <suwannss@ais.co.th>
2026-01-07 13:57:23 +0700

    fix: 🐛 asset path

--- next.config.js
+++ next.config.js
@@ -5,8 +5,7 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-    assetPrefix: '/admin',
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 module.exports = withNextIntl(nextConfig);
